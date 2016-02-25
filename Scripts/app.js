962fc6408d240550a852c7e3efb9f05a58bc4f0a
Esat IBIS <esat.taha.ibis@gmail.com>
2016-02-25 09:05:50 -0500

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,15 @@
+/**
+ * Author: Esat IBIS
+ * Date: 25/02/2016
+ * */
+// setup your IIFE (Immediately Invoked Function Expression)
+var travelReport = (function () {
+
+"use strict";
+console.log("App Started...")
+
+
+
+
+})();
+
